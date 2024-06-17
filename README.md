cb5745fb7dcafbc8b70a845eb4a5e2cfbe8c43b9
Raul <raul010280@gmail.com>
2024-06-17 01:58:44 -0400

    Terminamos la mayoria de CSS y HTML, Solamente falta el JS y luego modiificar CSS

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ScoreBoard
+
+
+This is a basketball Score board.
+
+Desing here. 
+https://www.figma.com/file/YC48MCx4frBFtYoz6rNJE6/Basketball-Scoreboard?node-id=107%3A195
+
+
---
+++ app.js
@@ -0,0 +1,20 @@
+let scoreNumberOne = document.getElementById("score-number1");
+let scoreNumberTwo = document.getElementById("score-number2");
+
+let btnOneOne = document.getElementById("score1-btn-1")
+let btnOneTwo = document.getElementById("score1-btn-2")
+let btnOneThree = document.getElementById("score1-btn-3")
+
+let btnTwoOne = document.getElementById("score2-btn-1")
+let btnTwoTwo = document.getElementById("score2-btn-2")
+let btnTwoThree = document.getElementById("score2-btn-3")
+
+function pointsOne(){
+
+}
+function pointsTwo(){
+
+}
+function pointsThree(){
+
+}
